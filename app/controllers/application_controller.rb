ff067aec4c97f1764bec206208a92abd372721ee
kak79 <kohelka@gmail.com>
2020-08-10 15:28:40 -0500

    application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "playmyboardgame"
+  end
+
+  get '/' do
+    erb :main
+  end 
+
+  helpers do
+    
+    def logged_in?
+      !!session[:username]
+    end
+
+    
+    def logout!
+      session.clear
+      redirect '/' 
+    end
+
+  end
+
+end
