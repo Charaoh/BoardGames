330266a163d58c66c92e3cf809d9410d88d6e2ed
kak79 <kohelka@gmail.com>
2020-08-11 14:58:17 -0500

    added user index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,16 @@ class GamesController < ApplicationController
         erb :'games/index'
     end
 
+    get '/games/user_index' do
+        @games = Game.find_by(user_id: params[:user_id])
+        erb :'games/user_index'
+    end
+
+    post '/games/user_index' do
+        @games = Game.find_by(user_id: params[:user_id])
+        
+    end
+
     get '/games/new' do
         erb :'games/new'
     end
@@ -18,27 +28,27 @@ class GamesController < ApplicationController
 
     get '/games/:id' do
         @game = Game.find_by(id: params[:id])
-        erb :'games/show'
-    end
-
-    post '/games/:id/show' do
-        @game = Game.find_by(id: params[:id])
-        if !Game.find_by(user_id: params[:user_id], id: params[:id])
-            Game.create(user_id: params[:user_id], id: params[:id])
-        else
-            redirect "/games/#{@game.id}"
-        end
-    end
-
-    get '/games/:id/play' do
-        @game = Game.find_by(id: params[:id])
         erb :'games/play'
     end
 
     post '/games/:id/play' do
-        @game = Game.find_by(id: params[:id])
         if !Game.find_by(user_id: params[:user_id], id: params[:id])
            redirect 'games/index' 
+        elsif Game.find_by(user_id: params[:user_id], id: params[:id])
+            redirect "/games/#{@game.id}"
+        end
+    end
+
+    get '/games/:id/delete' do
+        @game = Game.find_by(id: params[:id])
+        erb :'games/delete'        
+    end
+
+    post '/games/:id/delete' do
+        if !Game.find_by(user_id: params[:user_id], id: params[:id])
+            Game.drop(user_id: params[:user_id], id: params[:id])
+        else
+            erb :"games/invalid_delete"
         end
     end
  
