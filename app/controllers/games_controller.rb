7d651ddbff2da2e786ca708c558afc9877d95f78
kak79 <kohelka@gmail.com>
2020-08-11 13:42:37 -0500

    added a few routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,10 +21,25 @@ class GamesController < ApplicationController
         erb :'games/show'
     end
 
+    post '/games/:id/show' do
+        @game = Game.find_by(id: params[:id])
+        if !Game.find_by(user_id: params[:user_id], id: params[:id])
+            Game.create(user_id: params[:user_id], id: params[:id])
+        else
+            redirect "/games/#{@game.id}"
+        end
+    end
+
     get '/games/:id/play' do
         @game = Game.find_by(id: params[:id])
         erb :'games/play'
     end
 
+    post '/games/:id/play' do
+        @game = Game.find_by(id: params[:id])
+        if !Game.find_by(user_id: params[:user_id], id: params[:id])
+           redirect 'games/index' 
+        end
+    end
  
 end
